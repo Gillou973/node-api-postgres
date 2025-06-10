be0e4f111420e21b9f65f6486fd8ad76bebd32bd
Gilles GRIFFIT <griffitstudio@hotmail.com>
2025-06-10 11:47:44 -0300

    Add modif db.js

--- db.js
+++ db.js
@@ -2,11 +2,12 @@ require('dotenv').config(); // Ajoute cette ligne en haut
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.PGUSER,
+/*   user: process.env.PGUSER,
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  port: process.env.PGPORT, */
+  connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = pool;
